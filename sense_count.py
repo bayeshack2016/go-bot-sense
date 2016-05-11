704e26eb8300075a4641a31fb773423261c6ccf7
nicaford <aford@egov.com>
2016-05-10 20:17:46 -0500

    Both senses working from reference hardware.

---
+++ sense_count.py
@@ -0,0 +1,58 @@
+import sense
+import threading
+import RPi.GPIO as gpio
+
+#GPIO.wait_for_edge(21, GPIO.RISING)
+#GPIO.wait_for_edge(21, GPIO.FALLING)
+
+class CountSense(sense.Sense):
+	def __init__(self, pin_number=None, data_filename=None):
+		self.pin_number = pin_number
+		self.data_filename = data_filename
+		self.current_count = 0
+		self.monitor_thread = None
+	
+	def get_value(self):
+		return self.current_count
+	
+	def init(self,config):
+		if self.pin_number is None:
+			if len(config) > 0:
+				self.pin_number = config[0]
+		if self.data_filename is None:
+			if len(config) > 1:
+				self.data_filename = config[1]
+		try:
+			self.load_data_file()
+		except FileNotFoundError as fnfe:
+			self.current_count = 0
+		except:
+			raise	
+			
+		gpio.setmode(gpio.BCM)
+		gpio.setup(self.pin_number, gpio.IN)
+		
+		self.monitor_thread = CountMonitorThread(self)
+		self.monitor_thread.start()
+		
+	def load_data_file(self):
+		with open(self.data_filename) as data_file:
+			contents = data_file.read()
+			self.current_count = int(contents)
+	
+	def write_data_file(self):
+		with open(self.data_filename, "w") as data_file:
+			data_file.write(str(self.current_count))
+		
+class CountMonitorThread(threading.Thread):
+	def __init__(self, sense):
+		threading.Thread.__init__(self)
+		self.sense = sense
+	
+	def run(self):
+		while 1:
+			gpio.wait_for_edge(21, gpio.RISING)
+			self.sense.current_count += 1
+			self.sense.write_data_file()
+		
+		
---
+++ sense_temperature.py
@@ -0,0 +1,58 @@
+import sense
+import json
+import collections
+import Adafruit_ADS1x15
+
+class ConversionCurve:
+	def __init__(self, filename="temperature_curve.json", curve_points=None):
+		self.curve_points = curve_points
+		if curve_points is None:
+			self.load_curve(filename)
+	
+	def load_curve(self, filename):
+		raw_points = {}
+		with open(filename) as data_file:
+			jsondata = json.load(data_file)
+			for data in jsondata:
+				raw_points[data['V']] = data['T']
+		self.curve_points =  collections.OrderedDict(sorted(raw_points.items(), key=lambda t: t[0]))
+		
+	def convert(self, adcval):
+		first = True
+		lastval = -1
+		lasttemp = -1
+		for val, temp in self.curve_points.items():
+			if (lastval < 0):
+				if (adcval < val):
+					raise ValueError('Sensor value too small')
+			else:
+				if (adcval < val):
+					return lasttemp + ((adcval-lastval)/(val - lastval)) * (temp - lasttemp)
+			lastval = val
+			lasttemp = temp
+		raise ValueError('Sensor value too large')
+
+class TemperatureSense(sense.Sense):
+	def __init__(self, curve_filename=None, i2c_address=None, adc_channel=0):
+		self.curve_filename = curve_filename
+		self.curve = None
+		self.i2c_address = i2c_address
+		self.adc = None
+		self.adc_channel = adc_channel
+		self.adc_gain = 1.0
+	
+	def get_value(self):
+		volts = self.adc.read_adc(self.adc_channel, gain=self.adc_gain)
+		return self.curve.convert(volts)
+	
+	def init(self,config):
+		if self.i2c_address is None:
+			if len(config) > 0:
+				self.i2c_address = config[0]
+		if self.curve_filename is None:
+			if len(config) > 1:
+				self.curve_filename = config[1]
+		self.curve = ConversionCurve(filename=self.curve_filename)
+		self.adc = Adafruit_ADS1x15.ADS1115(address=self.i2c_address)
+		
+		
